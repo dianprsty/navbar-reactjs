ba946a0172a89dd53ceeccab33b6b31201afa401
dianp <dprasmail@gmail.com>
2023-05-31 14:14:17 +0700

    login & register button

--- src/components/layouts/NavbarLayouts.jsx
+++ src/components/layouts/NavbarLayouts.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import NavItem from "../fragments/NavItem";
+import Button from "../fragments/Button";
 
 const NavbarLayouts = () => {
   return (
     <>
-      <div className="bg-white w-full px-12 shadow-lg flex justify-between items-center py-4">
-        <div className="w-40 object-cover">
+      <div className="flex items-center justify-between w-full gap-12 px-12 py-4 bg-white shadow-lg">
+        <div className="object-cover w-40">
           <img src="/refactory-logo.png" />
         </div>
-        <div>
-          <ul className="flex items-center justify-end gap-8 text-lg">
+        <div className="flex-auto">
+          <ul className="flex items-center justify-end gap-8 text-lg grow">
             <NavItem>Home</NavItem>
             <NavItem>Services</NavItem>
             <NavItem>About Us</NavItem>
             <NavItem>Contacts</NavItem>
           </ul>
+        </div>
+        <div className="flex items-center justify-center gap-4">
+          <Button className={"bg-blue-500 text-white border-2 border-blue-500"}>
+            Login
+          </Button>
+          <Button className={"border-2 border-blue-500 text-blue-500"}>
+            Register
+          </Button>
         </div>
       </div>
     </>
